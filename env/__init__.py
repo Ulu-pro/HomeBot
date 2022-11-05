38a7dda4d5fc049666014cce761c9e81a7995515
Ulu-pro <web.ulu7@gmail.com>
2022-11-05 17:57:04 +0500

    Environment data for the Interface

---
+++ env/__init__.py
@@ -0,0 +1,18 @@
+class Env:
+    TOKEN = 'TgBotToken'
+    PORT = 'COM3'
+
+    TURN_ON = 'Turn on'
+    TURN_OFF = 'Turn off'
+
+    TURNED_ON = '%s is on'
+    TURNED_OFF = '%s is off'
+
+    CHOOSE = 'Choose LED:'
+    LEDS = ('R', 'Y', 'G', 'B', 'W')
+
+    R = '🔴'
+    Y = '🟡'
+    G = '🟢'
+    B = '🔵'
+    W = '⚪'
